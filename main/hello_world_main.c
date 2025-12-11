73b42a6157f80c6d0ff5b76df5165af8810e83de
Houwen <883681@fontys.nl>
2025-12-11 22:11:13 +0100

    Logging

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -15,11 +15,14 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
 
 #include <driver/temperature_sensor.h>
+
+static const char *TAG = "example";
 
 /* Services */
 #include "radio.h"
@@ -30,25 +33,29 @@
 void app_main(void)
 {
 
+    ESP_LOGI(TAG, "Install temperature sensor, expected temp ranger range: 10-50 °C");
     // int                         i; /* loop variable and temporary variable for return value */
     // esp_err_t                   esp_err;
     temperature_sensor_handle_t temp_sensor = NULL;
-    float tsens_out;
 
     /* Initialize temperature sensor (before running the pkt fwd thread) */
-    temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT( -10, 80 );
+    temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT( 10, 50 );
     ESP_ERROR_CHECK( temperature_sensor_install( &temp_sensor_config, &temp_sensor ) );
+
+    ESP_LOGI(TAG, "Enable temperature sensor");
     ESP_ERROR_CHECK( temperature_sensor_enable( temp_sensor ) );
 
     /* Start Radio */
     // launch_radio( temp_sensor );
 
+    ESP_LOGI(TAG, "Read temperature");
+    float tsens_out;
     while (1)
     {
         ESP_ERROR_CHECK(temperature_sensor_get_celsius(temp_sensor, &tsens_out));
 
-        printf("Hello world!\n");
-        printf("Temperature in %f °C\n", tsens_out);
+        // printf("Hello world!\n");
+        ESP_LOGI(TAG, "Temperature value %.02f °C", tsens_out);
 
         vTaskDelay(CONFIG_PAUSE / portTICK_PERIOD_MS);
     }
